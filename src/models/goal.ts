d07bc9c45bdb5d58e99c3ef09be17b74c3bc056d
Asli Athman <asliathman@gmail.com>
2022-01-28 13:20:46 -0500

    created a goals model and get goals and get goals/id routes, tested in Postman

---
+++ src/models/goal.ts
@@ -0,0 +1,34 @@
+import { Model, Sequelize, DataTypes, IntegerDataType } from 'sequelize';
+
+export default class Goal extends Model {
+    public id?: IntegerDataType;
+    public title!: string;
+    public description?: string;
+    public createdAt?: Date;
+}
+
+export const GoalMap = (sequelize: Sequelize) => {
+    Goal.init({
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING(255)
+        },
+        description: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: true
+        }
+    }, {
+        sequelize,
+        tableName: "goals",
+        timestamps: false
+    });
+    Goal.sync();
+}
--- src/routes/goals.routes.ts
+++ src/routes/goals.routes.ts
@@ -1,25 +1,31 @@
 import { Router, Request, Response } from 'express';
+import Goal, { GoalMap } from '../models/goal';
+import database from '../database';
 
 const router = Router();
 
 // GET - goals
 router.get('/', async (req: Request, res: Response) => {
-    // TO DO
-    const result : string[] = [];
+    GoalMap(database);
+    const result = await Goal.findAll();
     res.status(200).json({ goals: result });
 });
 
 // GET - goals/:id
 router.get('/:id', async (req: Request, res: Response) => {
-    // TO DO
-    const result : string = '';
+    GoalMap(database);
+    const id = Number(req.params.id);
+    const result = await Goal.findByPk(id);
     res.status(200).json({ goal: result });
 });
 
 // POST - goals
-router.post('/', async (req: Request, res: Response) => {
-    // TO DO
-    res.status(201).json({ goal: {} });
-});
+// router.post('/', async (req: Request, res: Response) => {
+//     let newGoal = req.body as Goal;
+//     GoalMap(database);
+//     const result = await Goal.create(newGoal);
+//     newGoal = result.dataValues as Goal;
+//     res.status(201).json({ goal: newGoal });
+// });
 
 export default router;
