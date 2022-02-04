110af3616947f43900a174f021e7e29301c4b23c
Asli Athman <asliathman@gmail.com>
2022-02-04 01:29:10 -0500

    added cors to fix network error in frontend

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { port } from './config';
 import goalsRoutes from './routes/goals.routes';
 
 const app = express();
+var cors = require('cors');
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
